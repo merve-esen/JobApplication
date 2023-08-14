cfd70691130c1b6fc123a271c333fe3cb026c5f6
merve-esen <merveesen834@gmail.com>
2023-08-14 23:03:01 +0300

    Add new test cases

--- JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -24,5 +24,42 @@ namespace JobApplicationLibrary.UnitTest
             // Assert
             Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
         }
+
+        [Test]
+        public void Application_WithNoTechStack_TransferredToAutoRejected()
+        {
+            // Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant { Age = 19 },
+                TechStackList = new List<string>() { "" }
+            };
+
+            // Action
+            var appResult = evaluator.Evaulate(form);
+
+            // Assert
+            Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
+        }
+
+        [Test]
+        public void Application_WithTechStackOver75P_TransferredToAutoAccepted()
+        {
+            // Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant { Age = 19 },
+                TechStackList = new List<string>() { "C#", "RabitMQ", "Microservice", "Visual Studio" },
+                YearsOfExperience = 16
+            };
+
+            // Action
+            var appResult = evaluator.Evaulate(form);
+
+            // Assert
+            Assert.AreEqual(ApplicationResult.AutoAccepted, appResult);
+        }
     }
 }
--- JobApplicationLibrary/ApplicationEvaluator.cs
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -5,13 +5,27 @@ namespace JobApplicationLibrary
     public class ApplicationEvaluator
     {
         private const int minAge = 18;
+        private const int autoAcceptedYearsOfExperience = 15;
+        private List<string> techStackList = new() { "C#", "RabitMQ", "Microservice", "Visual Studio" };
 
         public ApplicationResult Evaulate(JobApplication form)
         {
             if (form.Applicant.Age < minAge)
                 return ApplicationResult.AutoRejected;
-            
+
+            var similarityRate = GetTechStackSimilarityRate(form.TechStackList);
+            if(similarityRate < 25)
+                return ApplicationResult.AutoRejected;
+
+            if (similarityRate > 75 && form.YearsOfExperience >= autoAcceptedYearsOfExperience)
+                return ApplicationResult.AutoAccepted;
+
             return ApplicationResult.AutoAccepted;
+        }
+
+        private int GetTechStackSimilarityRate(List<string> techStacks) {
+            var matchedCount = techStacks.Where(i => techStackList.Contains(i, StringComparer.OrdinalIgnoreCase)).Count();
+            return (int)((double)matchedCount / techStackList.Count) * 100;
         }
     }
 
